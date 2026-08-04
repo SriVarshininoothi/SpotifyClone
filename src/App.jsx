441862837bb456f3a03be5e736f047d20d0b64f6
SriVarshininoothi <noothi.srivarshini982@gmail.com>
2026-08-04 13:44:56 +0530

    Created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import Sidebar  from './Components/Sidebar'
 import Player from './Components/Player'
+import Display from './Components/Display'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex' >
         <Sidebar/>
+        <Display/>
 
       </div>
       <Player/>
