a565ee5ffab5c0fd1e93ee5170d6739beb16e686
SriVarshininoothi <noothi.srivarshini982@gmail.com>
2026-08-05 15:33:38 +0530

    Display and DisplayHome was updated

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import {Routes,Route }from 'react-router-dom'
 import DisplayHome from './DisplayHome'
+import AlbumItem from './AlbumItem'
+import SongsData from './SongsData'
+import DisplayAlbum from './DisplayAlbum'
 
 const Display = () => {
   return (
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
         <Routes>
-            <Route path="/" 
-            element={<DisplayHome/> }></Route>
+            <Route path="/" element={<DisplayHome/> }></Route>
+            <Route path='/album/:id' element={<DisplayAlbum/>}/>
+            <Route path='/song/:id' element={<SongsData/>}/>
         </Routes>
     </div>
   )
--- src/Components/DisplayHome.jsx
+++ src/Components/DisplayHome.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { albumsData } from '../assets/assets'
+import AlbumItem from './AlbumItem'
+import { songsData } from '../assets/assets'
+import SongsData from './SongsData'
 
 const DisplayHome = () => {
   return (
     <>
         <Navbar/>
+        {/* album data */}
+
+        <div className='mb-4'>
+          <h3 className='font-bold text-2xl my-5 flex items-start'>Featured Charts</h3>
+          <div className='flex overflow-auto'>
+          {
+              albumsData.map((item,index)=>
+                (<AlbumItem key={index} 
+                  name={item.name}
+                   desc={item.desc} 
+                   id={item.id}
+                    image={item.image}/>))
+             }    
+          </div>
+             
+        </div>
+
+      {/* songs data */}
+
+         <div className='mb-4'>
+          <h3 className='font-bold text-2xl my-5 flex items-start'>Today's biggest hits</h3>
+          <div className='flex overflow-auto'>
+          {
+            songsData.map((item,index)=>(
+              <SongsData key={index} name={item.name} image={item.image} desc={item.desc} id={item.id}/>
+            ))
+              
+          }
+          </div>
+             
+        </div>
     </>
   )
 }
